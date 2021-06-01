eeb5b60dcae6633c78a9d89b5a0db395367e00d4
MarlonnCarvalhosa <marlonncarvalhosa@hotmail.com>
2021-06-01 20:01:22 -0300

    Added Floating Buttom Menu, SheetBottom and Login whit Google.

---
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/DialogLogin.kt
@@ -0,0 +1,72 @@
+package com.marlonncarvalhosa.covidmap
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class DialogLogin: DialogFragment() {
+
+    var googleSignClient : GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView: View = inflater.inflate(R.layout.dialog_login, container, false)
+
+        //Codigos
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignClient = GoogleSignIn.getClient(rootView.context,gso)
+
+        val signInIntent = googleSignClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        return rootView
+    }
+
+    fun firebaseAuthWithGoogle(acct : GoogleSignInAccount?) {
+        val credencial = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credencial).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Login efetuado com sucesso.", Toast.LENGTH_LONG).show()
+                dialog?.dismiss()
+            } else {
+                Toast.makeText(context, "Algo de errado no login", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Log.e("DIALOGLOGIN", "Google SingIn Faield", e)
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/marlonncarvalhosa/covidmap/MapsActivity.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/MapsActivity.kt
@@ -1,30 +1,52 @@
 package com.marlonncarvalhosa.covidmap
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.*
-import com.google.android.gms.maps.*
-
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.marlonncarvalhosa.covidmap.databinding.ActivityMapsBinding
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_maps.*
+import kotlinx.android.synthetic.main.activity_profile_sheet_dialog.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var firebaseAuth: FirebaseAuth
     private val LOCATION_PERMISSION_REQUEST = 1
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
+    private var user = FirebaseAuth.getInstance().currentUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -35,10 +57,54 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        sheetDialog()
 
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    private fun sheetDialog(){
+        fb_profile.setOnClickListener {
+
+            user = FirebaseAuth.getInstance().currentUser
+
+            if(firebaseAuth.currentUser != null) {
+
+                user = FirebaseAuth.getInstance().getCurrentUser();
+
+                val bottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme);
+                val bottomSheetView = LayoutInflater.from(applicationContext).inflate(R.layout.activity_profile_sheet_dialog,
+                    findViewById<ConstraintLayout>(R.id.bottomSheetContainer)
+                )
+
+                Log.d("USER", user?.photoUrl.toString())
+                Log.d("USER", user?.displayName.toString())
+                Log.d("USER", user?.email.toString())
+                Log.d("USER", user?.uid.toString())
+
+                var teste = user?.displayName
+                tv_teste.text = teste
+                //Toast.makeText(this, teste, Toast.LENGTH_LONG).show()
+
+                //Picasso.get().load(firebaseAuth.currentUser!!.photoUrl).fit().centerCrop().into(civ_profile_pic)
+
+//                sign_in_button.setOnClickListener {
+//                    bottomSheetDialog.dismiss()
+//                }
+
+                bottomSheetDialog.setContentView(bottomSheetView)
+                bottomSheetDialog.show()
+            } else {
+                val dialog = DialogLogin()
+                dialog.show(supportFragmentManager, "DialogLogin")
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LOCATION_PERMISSION_REQUEST) {
             if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
@@ -54,15 +120,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
                 mMap.isMyLocationEnabled = true
                 fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
-                    if (location != null){
+                    if (location != null) {
                         val currentLatLong = LatLng(location.latitude, location.longitude)
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 15f))
                     }
                 }
 
-            }
-            else {
-                Toast.makeText(this, "O usuário não concedeu permissão de acesso ao local", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "O usuário não concedeu permissão de acesso ao local",
+                    Toast.LENGTH_LONG
+                ).show()
                 finish()
             }
         }
@@ -75,13 +144,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun getLocationAccess() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             mMap.isMyLocationEnabled = true
             getLocationUpdates()
             startLocationUpdates()
-        }
-        else
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST)
+        } else
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST
+            )
     }
 
     private fun getLocationUpdates() {
