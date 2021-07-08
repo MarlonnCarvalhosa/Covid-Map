9e40f188c5ca257a303d6534107402bf900c6d0d
Matheus Lima <matheusldasilva20088@gmail.com>
2021-07-08 20:07:57 -0300

    adiciona click listener para eventos da view e refatora tela para view binding

--- app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/FirstSymptomSessionFragment.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/FirstSymptomSessionFragment.kt
@@ -7,44 +7,55 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.marlonncarvalhosa.covidmap.R
+import com.marlonncarvalhosa.covidmap.databinding.FragmentFirstSymptomSessionBinding
 
 class FirstSymptomSessionFragment: Fragment(), View.OnClickListener {
+
+    private var binding : FragmentFirstSymptomSessionBinding? = null // inicializar como null para utilizar o null safety e garantir que a aplicação não vá quebrar caso alguma view seja nula
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val view: View = inflater.inflate(R.layout.fragment_first_symptom_session, container, false)
+    ): ConstraintLayout? {
+        binding = FragmentFirstSymptomSessionBinding.inflate(layoutInflater)
+        return binding?.root
+    }
 
-        view.findViewById<CardView>(R.id.cv_first_next).setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListeners()
+        binding?.cvFirstNext?.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.quiz_container, SecondSymptomSessionFragment())
                 .addToBackStack("first")
                 .commit()
         }
-
-        view.findViewById<CardView>(R.id.cv_first_back).setOnClickListener {
-            parentFragmentManager.popBackStack("start", 1)
-        }
-
-        val teste1 = view.findViewById<CardView>(R.id.cv_covid_yes)
-        teste1.setOnClickListener {this}
-
-        return view
+        binding?.cvFirstBack?.setOnClickListener { parentFragmentManager.popBackStack("start", 1) }
     }
-
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.cv_covid_yes -> {
-                Log.d("TESTE", "COVIDDDDDDDDDDDDDDD")
-                Toast.makeText(context, "Teste", Toast.LENGTH_LONG).show()
+            binding?.buttonPositiveCovidTest?.id -> {
+                Log.d("teste", "positive click")
+                Toast.makeText(context, "Positive click", Toast.LENGTH_LONG).show()
             }
-
-            else -> {
+            binding?.buttonNegativeCovidTest?.id -> {
+                Log.d("teste", "negative click")
+                Toast.makeText(context, "negative click", Toast.LENGTH_LONG).show()
             }
+            else -> { }
         }
+    }
+    private fun setClickListeners() {
+        binding?.buttonPositiveCovidTest?.setOnClickListener(this)
+        binding?.buttonNegativeCovidTest?.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null //retirar a referencia de view binding para evitar memory leak
     }
 }
