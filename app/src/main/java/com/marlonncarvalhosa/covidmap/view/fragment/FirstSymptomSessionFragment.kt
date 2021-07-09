bb9ea0d6885bc619b14b6e8b565818854c0d2cdb
MarlonnCarvalhosa <marlonncarvalhosa@hotmail.com>
2021-07-09 09:20:16 -0300

    Refatorando ids.

--- app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/FirstSymptomSessionFragment.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/FirstSymptomSessionFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.marlonncarvalhosa.covidmap.R
@@ -14,7 +13,8 @@ import com.marlonncarvalhosa.covidmap.databinding.FragmentFirstSymptomSessionBin
 
 class FirstSymptomSessionFragment: Fragment(), View.OnClickListener {
 
-    private var binding : FragmentFirstSymptomSessionBinding? = null // inicializar como null para utilizar o null safety e garantir que a aplicação não vá quebrar caso alguma view seja nula
+    // inicializar como null para utilizar o null safety e garantir que a aplicação não vá quebrar caso alguma view seja nula
+    private var binding: FragmentFirstSymptomSessionBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,30 +28,31 @@ class FirstSymptomSessionFragment: Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setClickListeners()
-        binding?.cvFirstNext?.setOnClickListener {
+        binding?.buttonFirstNext?.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.quiz_container, SecondSymptomSessionFragment())
                 .addToBackStack("first")
                 .commit()
         }
-        binding?.cvFirstBack?.setOnClickListener { parentFragmentManager.popBackStack("start", 1) }
+        binding?.buttonFirstBack?.setOnClickListener { parentFragmentManager.popBackStack("start", 1) }
     }
     override fun onClick(v: View?) {
         when (v?.id) {
-            binding?.buttonPositiveCovidTest?.id -> {
+            binding?.buttonPositiveCovid?.id -> {
                 Log.d("teste", "positive click")
                 Toast.makeText(context, "Positive click", Toast.LENGTH_LONG).show()
             }
-            binding?.buttonNegativeCovidTest?.id -> {
+            binding?.buttonNegativeCovid?.id -> {
                 Log.d("teste", "negative click")
                 Toast.makeText(context, "negative click", Toast.LENGTH_LONG).show()
             }
-            else -> { }
+            else -> {
+            }
         }
     }
     private fun setClickListeners() {
-        binding?.buttonPositiveCovidTest?.setOnClickListener(this)
-        binding?.buttonNegativeCovidTest?.setOnClickListener(this)
+        binding?.buttonPositiveCovid?.setOnClickListener(this)
+        binding?.buttonNegativeCovid?.setOnClickListener(this)
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/SecondSymptomSessionFragment.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/SecondSymptomSessionFragment.kt
@@ -4,30 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.marlonncarvalhosa.covidmap.R
+import com.marlonncarvalhosa.covidmap.databinding.FragmentSecondSymptomSessionBinding
 
-class SecondSymptomSessionFragment: Fragment(){
+class SecondSymptomSessionFragment : Fragment(), View.OnClickListener {
+
+    // inicializar como null para utilizar o null safety e garantir que a aplicação não vá quebrar caso alguma view seja nula
+    private var binding: FragmentSecondSymptomSessionBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val view: View = inflater.inflate(R.layout.fragment_second_symptom_session, container, false)
+    ): ConstraintLayout? {
+        binding = FragmentSecondSymptomSessionBinding.inflate(layoutInflater)
+        return binding?.root
+    }
 
-        view.findViewById<CardView>(R.id.cv_second_next).setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListeners()
+        binding?.cvSecondNext?.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.quiz_container, ThirdSymptomSessionFragment())
                 .addToBackStack("second")
                 .commit()
         }
+        binding?.cvSecondBack?.setOnClickListener { parentFragmentManager.popBackStack("first", 1) }
+    }
 
-        view.findViewById<CardView>(R.id.cv_second_back).setOnClickListener {
-            parentFragmentManager.popBackStack("first", 1)
-        }
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
 
-        return view
+    private fun setClickListeners() {
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null //retirar a referencia de view binding para evitar memory leak
     }
 }
