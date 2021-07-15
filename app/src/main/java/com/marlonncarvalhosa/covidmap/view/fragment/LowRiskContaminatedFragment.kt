f910a56b0380a580ec1f843d0911ab6b0c49042b
MarlonnCarvalhosa <marlonncarvalhosa@hotmail.com>
2021-07-15 17:20:15 -0300

    Começando a tela do baixo risco.

--- app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/LowRiskContaminatedFragment.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/LowRiskContaminatedFragment.kt
@@ -1,21 +1,36 @@
 package com.marlonncarvalhosa.covidmap.view.fragment
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.bold
 import androidx.fragment.app.Fragment
-import com.marlonncarvalhosa.covidmap.R
+import com.marlonncarvalhosa.covidmap.databinding.FragmentLowRiskContaminatedBinding
 
 class LowRiskContaminatedFragment : Fragment() {
+
+    private var binding: FragmentLowRiskContaminatedBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_low_risk_contaminated, container, false)
+    ): ConstraintLayout? {
+        binding = FragmentLowRiskContaminatedBinding.inflate(layoutInflater)
+        val lowRiskOrientation = SpannableStringBuilder()
+            .append("Baseado em suas respostas, é possível que essa situação ")
+            .bold { append("não") }
+            .append(" se enquadre como caso suspeito de sintomático para Coronavírus 2019 (Covid-19)." +
+                    "\n\nMantenha as condutas de precaução e prevenção, praticando a etiqueta respiratória.")
+        binding?.txtLowRiskOrientation?.text = lowRiskOrientation
 
-        return root
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null //retirar a referencia de view binding para evitar memory leak
     }
 }
