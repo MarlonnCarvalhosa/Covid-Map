d8b19560c0d5e0a615b1b2ddb3423ab70b42d61e
MarlonnCarvalhosa <marlonncarvalhosa@hotmail.com>
2021-07-19 11:36:17 -0300

    Finalizando a tela de High risk.

--- app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/HighRiskContaminatedFragment.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/view/fragment/HighRiskContaminatedFragment.kt
@@ -1,21 +1,44 @@
 package com.marlonncarvalhosa.covidmap.view.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import com.marlonncarvalhosa.covidmap.R
+import com.marlonncarvalhosa.covidmap.databinding.FragmentHighRiskContaminatedBinding
 
-class HighRiskContaminatedFragment : Fragment() {
+
+class HighRiskContaminatedFragment : Fragment(){
+
+    private var binding: FragmentHighRiskContaminatedBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_high_risk_contaminated, container, false)
+    ): ConstraintLayout? {
+        binding = FragmentHighRiskContaminatedBinding.inflate(layoutInflater)
+        call()
+        startWhatsapp()
+        return binding?.root
+    }
 
-        return root
+    private fun call() {
+        binding?.cvCall?.setOnClickListener {
+            val uri: Uri = Uri.parse("tel:$136")
+            val intent = Intent(Intent.ACTION_DIAL, uri)
+            startActivity(intent)
+        }
+    }
+
+    private fun startWhatsapp(){
+        binding?.cvWpp?.setOnClickListener {
+            val uriWpp: Uri = Uri.parse("https://api.whatsapp.com/send?phone=556199380031&text=oi&source=&data=")
+            val intentWpp = Intent(Intent.ACTION_VIEW, uriWpp)
+            startActivity(intentWpp)
+        }
+
     }
 }
