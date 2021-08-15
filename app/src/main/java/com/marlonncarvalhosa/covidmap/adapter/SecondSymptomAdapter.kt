9a28033831f38c8fc8b77adc6677cbbfa60744bb
Matheus Lima <matheusldasilva20088@gmail.com>
2021-08-15 20:55:17 -0300

    remove all synthoms

--- app/src/main/java/com/marlonncarvalhosa/covidmap/adapter/SecondSymptomAdapter.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/adapter/SecondSymptomAdapter.kt
@@ -1,18 +1,23 @@
 package com.marlonncarvalhosa.covidmap.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.marlonncarvalhosa.covidmap.R
 import com.marlonncarvalhosa.covidmap.databinding.ItemSymptomBinding
 import com.marlonncarvalhosa.covidmap.model.SecondSymptomModel
 
 class SecondSymptomAdapter(
     private val onSymtomSelectedListener : (SecondSymptomModel) -> Unit,
-    private val onSymptomDesselectedListener: (SecondSymptomModel) -> Unit,)
+    private val onSymptomDesselectedListener: (SecondSymptomModel) -> Unit)
     : RecyclerView.Adapter<SecondSymptomAdapter.ViewHolder>() {
 
     private var secondSymptomModel: List<SecondSymptomModel> = ArrayList()
     lateinit var binding: ItemSymptomBinding
+    private val holders : MutableList<SecondSymptomAdapter.ViewHolder> = ArrayList()
+
     inner class ViewHolder(binding: ItemSymptomBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(secondSymptomModel: SecondSymptomModel, position: Int) {
             binding.materialCheckBox.text = secondSymptomModel.secondSymptomName
@@ -25,14 +30,31 @@ class SecondSymptomAdapter(
     }
 
     override fun onBindViewHolder(holder: SecondSymptomAdapter.ViewHolder, position: Int) {
+        holders.add(holder)
         val secondSymptomModel = secondSymptomModel[position]
         holder.bind(secondSymptomModel, position)
+      if (secondSymptomModel.secondSymptomName == "Nenhum desses sintomas") { binding.materialCheckBox.tag = "REMOVEALL" }
+
         binding.materialCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
-            when(isChecked) {
-                true -> onSymtomSelectedListener(secondSymptomModel)
-                false -> onSymptomDesselectedListener(secondSymptomModel)
+            buttonView.apply {
+                when(tag) {
+                    "REMOVEALL" -> onRemoveAllSelected()
+                    else -> checkStatus(isChecked, secondSymptomModel)
+                }
             }
         }
+    }
+
+    private fun checkStatus(checked: Boolean, secondSymptomModel: SecondSymptomModel) {
+        when(checked) {
+            true -> onSymtomSelectedListener(secondSymptomModel)
+            false -> onSymptomDesselectedListener(secondSymptomModel)
+        }
+
+    }
+
+    private fun onRemoveAllSelected() {
+        holders.forEach { it.itemView.findViewById<MaterialCheckBox>(R.id.materialCheckBox).isChecked = false }
     }
 
     override fun getItemCount(): Int = secondSymptomModel.size
