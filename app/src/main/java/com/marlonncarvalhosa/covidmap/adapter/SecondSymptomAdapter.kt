fa514d0ae51d6b7ebc17c375773200b71c706ba9
MarlonnCarvalhosa <marlonncarvalhosa@hotmail.com>
2021-08-16 17:52:06 -0300

    Desativando os outros botões no click do sem sintomas.

--- app/src/main/java/com/marlonncarvalhosa/covidmap/adapter/SecondSymptomAdapter.kt
+++ app/src/main/java/com/marlonncarvalhosa/covidmap/adapter/SecondSymptomAdapter.kt
@@ -1,8 +1,10 @@
 package com.marlonncarvalhosa.covidmap.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.checkbox.MaterialCheckBox
 import com.marlonncarvalhosa.covidmap.R
@@ -10,13 +12,13 @@ import com.marlonncarvalhosa.covidmap.databinding.ItemSymptomBinding
 import com.marlonncarvalhosa.covidmap.model.SecondSymptomModel
 
 class SecondSymptomAdapter(
-    private val onSymtomSelectedListener : (SecondSymptomModel) -> Unit,
-    private val onSymptomDesselectedListener: (SecondSymptomModel) -> Unit)
-    : RecyclerView.Adapter<SecondSymptomAdapter.ViewHolder>() {
+    private val onSymtomSelectedListener: (SecondSymptomModel) -> Unit,
+    private val onSymptomDesselectedListener: (SecondSymptomModel) -> Unit
+) : RecyclerView.Adapter<SecondSymptomAdapter.ViewHolder>() {
 
     private var secondSymptomModel: List<SecondSymptomModel> = ArrayList()
     lateinit var binding: ItemSymptomBinding
-    private val holders : MutableList<SecondSymptomAdapter.ViewHolder> = ArrayList()
+    private val holders: MutableList<SecondSymptomAdapter.ViewHolder> = ArrayList()
 
     inner class ViewHolder(binding: ItemSymptomBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(secondSymptomModel: SecondSymptomModel, position: Int) {
@@ -24,9 +26,12 @@ class SecondSymptomAdapter(
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SecondSymptomAdapter.ViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SecondSymptomAdapter.ViewHolder {
         binding = ItemSymptomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return  ViewHolder(binding)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: SecondSymptomAdapter.ViewHolder, position: Int) {
@@ -37,7 +42,7 @@ class SecondSymptomAdapter(
 
         binding.materialCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
             buttonView.apply {
-                when(tag) {
+                when (tag) {
                     "REMOVEALL" -> onRemoveAllSelected(this)
                     else -> checkStatus(isChecked, secondSymptomModel)
                 }
@@ -47,16 +52,27 @@ class SecondSymptomAdapter(
 
     private fun onRemoveAllSelected(compoundButton: CompoundButton?) {
         if (binding.materialCheckBox.isChecked) {
-            holders.forEach { it.itemView.findViewById<MaterialCheckBox>(R.id.materialCheckBox).isChecked = false }
+            holders.forEach {
+                it.itemView.findViewById<MaterialCheckBox>(R.id.materialCheckBox).isChecked = false
+            }
             compoundButton?.isChecked = true
+            holders.forEach {
+                it.itemView.findViewById<MaterialCheckBox>(R.id.materialCheckBox).isEnabled = false
+            }
+        } else {
+            compoundButton?.isChecked = false
+            holders.forEach {
+                it.itemView.findViewById<MaterialCheckBox>(R.id.materialCheckBox).isEnabled = true
+            }
         }
     }
 
     private fun checkStatus(checked: Boolean, secondSymptomModel: SecondSymptomModel) {
-        when(checked) {
+        when (checked) {
             true -> onSymtomSelectedListener(secondSymptomModel)
             false -> onSymptomDesselectedListener(secondSymptomModel)
         }
+
     }
 
     override fun getItemCount(): Int = secondSymptomModel.size
